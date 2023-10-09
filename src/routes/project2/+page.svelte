91625e18efaf95f1708980acb0a60aef88a81533
pepefrog <santiihulk@gmail.com>
2023-10-08 19:42:57 -0700

    a

---
+++ src/routes/project2/+page.svelte
@@ -0,0 +1,14 @@
+<style>
+    body {
+         background-image: url('./3.png');
+         background-repeat: no-repeat;
+         background-size: cover;
+         height :100vh;
+         width:100%;
+         object-fit:cover;
+   }
+ </style>
+ 
+ <body>
+   
+ </body>
